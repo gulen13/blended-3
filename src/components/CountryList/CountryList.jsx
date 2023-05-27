b1249888eee9a48877e4405220f0e6bf33ea2105
Nicholas <nicholasmoskalenko@gmail.com>
2023-05-27 12:22:49 +0300

    task-5

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,12 +1,13 @@
 import { Grid, GridItem } from 'components';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const CountryList = ({ countries }) => {
+  const location = useLocation();
   return (
     <Grid>
       {countries.map(country => (
         <GridItem key={country.id}>
-          <Link to={`/country/${country.id}`}>
+          <Link to={`/country/${country.id}`} state={{ from: location }}>
             <img src={country.flag} alt={country.country} />
           </Link>
         </GridItem>
---
+++ src/components/GoBackLink/GoBackLink.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+export const GoBackLink = ({ path }) => {
+  return (
+    <div
+      style={{
+        marginBottom: '60px',
+        color: '#f2f2f2',
+        letterSpacing: '0.06em',
+        textDecoration: 'underline',
+
+        borderColor: 'gray',
+      }}
+    >
+      <Link to={path}>Back to Countries</Link>
+    </div>
+  );
+};
